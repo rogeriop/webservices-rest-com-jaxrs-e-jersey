ba6a7b1bcfd2750665abec394607b1b66979f76b
rogeriop <pinheiroxrogerio@gmail.com>
2016-01-11 15:07:34 -0300

    fim 01 - Servicos WEB REST e Adressabilitdy

---
+++ src/main/java/br/com/alura/loja/ProjetoTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProjetoTest {
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://127.0.0.1:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Minha loja"));
+	}
+}
